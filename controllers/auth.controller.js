cdfb7a07fdd797d40c5bceef515c5156352c2930
CriskunVN <phukun0404@gmail.com>
2025-07-02 15:43:09 +0700

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -138,7 +138,9 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3. Gửi reset token qua email
-  const resetURL = `http://localhost:3001/account/reset/password?token=${resetToken}`;
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
 
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
                     If you didn't forget your password, please ignore this email!`;
