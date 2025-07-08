e0542fcd1f2602771264fe955530e81c2ad03839
CriskunVN <phukun0404@gmail.com>
2025-07-08 13:06:29 +0700

    add middle protect , author.. for parkingSlot routes and vehicle routes

--- routes/parkingSlot.route.js
+++ routes/parkingSlot.route.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import * as authController from '../controllers/auth.controller.js';
 import * as ParkingSlot from '../controllers/parkingSlot.controller.js';
 const router = express.Router();
+
+// Dùng để bảo vệ tất cả các route bên dưới
+router.use(
+  authController.protect,
+  authController.restrictTo('admin', 'parking_owner')
+);
 
 router
   .route('/')
--- routes/vehicle.route.js
+++ routes/vehicle.route.js
@@ -1,18 +1,22 @@
-import express from "express";
+import express from 'express';
 import {
   getAllVehicles,
   getVehicleById,
   createVehicle,
   updateVehicle,
-  deleteVehicle
-} from "../controllers/vehicle.controller.js";
+  deleteVehicle,
+} from '../controllers/vehicle.controller.js';
+import * as authController from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get("/", getAllVehicles);
-router.get("/:id", getVehicleById);
-router.post("/", createVehicle);
-router.put("/:id", updateVehicle);
-router.delete("/:id", deleteVehicle);
+// Dùng để bảo vệ tất cả các route bên dưới
+router.use(authController.protect, authController.restrictTo('admin', 'user'));
+
+router.get('/', getAllVehicles);
+router.get('/:id', getVehicleById);
+router.post('/', createVehicle);
+router.put('/:id', updateVehicle);
+router.delete('/:id', deleteVehicle);
 
 export default router;
