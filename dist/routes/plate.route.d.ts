524cdb96c85b4b720e97a895ab695b26b2c025e5
boonboww <anhquan504@gmail.com>
2025-09-10 11:38:08 +0700

    update scan

---
+++ dist/routes/plate.route.d.ts
@@ -0,0 +1,3 @@
+export default router;
+declare const router: import("express-serve-static-core").Router;
+//# sourceMappingURL=plate.route.d.ts.map
---
+++ dist/services/licensePlate.service.d.ts
@@ -0,0 +1,2 @@
+export function recognizeLicensePlate(imageBuffer: any): Promise<string>;
+//# sourceMappingURL=licensePlate.service.d.ts.map
