d1ca8f36a6d457a9bafa1cb9981d9569c4a64657
boonboww <anhquan504@gmail.com>
2025-09-11 15:28:29 +0700

    update full

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ import vehicleRoutes from './routes/vehicle.route.js';
 import bookingRouter from './routes/booking.route.js';
 import chatbotRoutes from './routes/chatbot.routes.js';
 import ticketRouter from './routes/ticket.route.js';
-import plateRoutes from "./routes/plate.route.js";
+import ocrRoute from "./routes/ocr.route.js";
 // Route for VNPay
 import vnpayRouter from './routes/vnpay.route.js';
 import { fileURLToPath } from 'url';
@@ -26,14 +26,22 @@ const app = express();
 // 1. GLOBAL MIDDLEWARE
 // Cấu hình CORS để cho phép frontend truy cập vào backend
 app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'https://go-park-fe.vercel.app',
-    ],
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'https://go-park-fe.vercel.app',
+        ];
+        // Cho phép cả request không có origin (ví dụ từ Postman)
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
-    preflightContinue: false,
     optionsSuccessStatus: 200,
 }));
 app.use(helmet()); // Bảo mật HTTP headers
@@ -53,7 +61,7 @@ app.use(`${apiPrefix}/chatbot`, chatbotRoutes); // route chatbot
 app.use(`${apiPrefix}/tickets`, ticketRouter); //route vé xe
 app.use(`${apiPrefix}/vnpay`, vnpayRouter); // route thanh toán VNPay
 app.use(`${apiPrefix}/admin`, adminRouter); // route admin
-app.use("/api/v1/plate", plateRoutes); // route scan
+app.use(`${apiPrefix}/ocr`, ocrRoute); //route scan
 // 4. ERROR HANDLER (Middle xử lý lỗi)
 app.use((err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
